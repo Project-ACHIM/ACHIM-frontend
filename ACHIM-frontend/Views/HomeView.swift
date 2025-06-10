e0f913388dc18c54c487e68ac08ff5075ceb1eb1
UTakuto <taku.0307.ue@icloud.com>
2025-06-10 09:15:10 +0900

    CountdownCardViewを追加し、HomeViewに統合

--- ACHIM-frontend/Views/HomeView.swift
+++ ACHIM-frontend/Views/HomeView.swift
@@ -11,14 +11,8 @@ struct HomeView: View {
     @StateObject var viewModel = StepCounterViewModel()
     var body: some View {
         
-        VStack {
-            Text("朝活終了")
-                .frame(width: 350, height: 80)
-                .background(
-                    RoundedRectangle(cornerRadius: 15)
-                        .fill(Color.white)
-                        .shadow(color: Color("5D9FED").opacity(0.7), radius: 4, x: 0, y: 0)
-                )
+        VStack{
+            CountdownCardView()
             StepCountCardView(stepCount: viewModel.stepCount)
         }
         .onAppear {
