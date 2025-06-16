6e34ad89ea0a40632f81976a47ce746c9cf8a7e9
りんご <hanacoi7243@gmail.com>
2025-06-16 23:17:15 +0900

    [add]ranking my card

--- ACHIM-frontend/Views/ranking.swift
+++ ACHIM-frontend/Views/ranking.swift
@@ -9,55 +9,74 @@ import SwiftUI
 struct ranking: View {
     var body: some View {
         //全体のアイテム
-        VStack {
-            //タイトル
-            Text("ランキング")
-                .zenFont(.bold, size:32, color: Color.white)
-                .stroke(color: Color.hex5D9FED, width: 2)
-            
-            //画面サイズ取得
-            GeometryReader{ GeometryReader  in
-                let view_maxX = GeometryReader.frame(in: .local).maxX   //画面サイズ横
-                let View_maxY = GeometryReader.frame(in: .local).maxY   //画面サイズ縦
-                
-                VStack {
-                    ZStack(alignment: .top) {
-                        //ヘッダーアイテム
-                        Rankingheader()
-                        //線
-                        VStack(alignment: .trailing) {
-                            Spacer()
-                            RoundedRectangle(cornerRadius: 20)
-                                .frame(width: 350, height: 2)
-                                .foregroundStyle(Color.hex347CD1)
-                                .padding(.bottom, 30)
+        ZStack {
+            VStack {
+                Text("ランキング")
+                    .zenFont(.bold, size:32, color: Color.white)
+                    .stroke(color: Color.hex5D9FED, width: 2)
+
+                //画面サイズ取得
+                GeometryReader{ GeometryReader  in
+                    let view_maxX = GeometryReader.frame(in: .local).maxX   //画面サイズ横
+                    let View_maxY = GeometryReader.frame(in: .local).maxY   //画面サイズ縦
+                    
+                    VStack {
+                        ZStack(alignment: .top) {
+                            //ヘッダーアイテム
+                            Rankingheader()
+                            //線
+                            VStack(alignment: .trailing) {
+                                Spacer()
+                                RoundedRectangle(cornerRadius: 20)
+                                    .frame(width: 350, height: 2)
+                                    .foregroundStyle(Color.hex347CD1)
+                                    .padding(.bottom, 30)
+                            }
+                            .frame(height: 80)
+                            
+                            //背景
+                            Path{ Path in
+                                Path.move(to: CGPoint(x: 0, y: 50))
+                                Path.addLine(to: CGPoint(x: view_maxX, y: 50))
+                                Path.addLine(to: CGPoint(x: view_maxX, y: View_maxY))
+                                Path.addLine(to: CGPoint(x: 0, y: View_maxY))
+                            }
+                            .fill(Color.baseSky)
+                            //achi 手
+                            HStack(alignment: .firstTextBaseline) {
+                                Image(.achiArm)
+                                    .padding(.leading, 38)
+                                    .padding(.top, 44)
+                                Spacer()
+                            }
+                            .frame(width: view_maxX)
                         }
-                        .frame(height: 80)
-                        
-                        Path{ Path in
-                            Path.move(to: CGPoint(x: 0, y: 50))
-                            Path.addLine(to: CGPoint(x: view_maxX, y: 50))
-                            Path.addLine(to: CGPoint(x: view_maxX, y: View_maxY))
-                            Path.addLine(to: CGPoint(x: 0, y: View_maxY))
-                        }
-                        .fill(Color.baseSky)
-                        //achi 手
-                        HStack(alignment: .firstTextBaseline) {
-                            Image(.achiArm)
-                                .padding(.leading, 38)
-                                .padding(.top, 44)
-                            Spacer()
-                        }
-                        .frame(width: view_maxX)
                     }
                 }
-                
             }
-            Spacer()
+            //ランキング
+            VStack{
+                Spacer()
+    
+                VStack(alignment: .center, spacing: 20){
+                    RankingMyCard(userName: "マイネーム", rank: 5, sp: 500000)
+                    
+                    VStack(alignment: .center,spacing: 16){
+                        RankingCards(userName: "ライバルネーム", rank: 1, sp: 1000000)
+                        RankingCards(userName: "ライバルネーム", rank: 2, sp: 900000)
+                        RankingCards(userName: "ライバルネーム", rank: 3, sp: 800000)
+                        RankingCards(userName: "ライバルネーム", rank: 4, sp: 700000)
+                        RankingCards(userName: "ライバルネーム", rank: 5, sp:500000)
+                    }
+                    .frame(width: 400)
+                    nav_bar()
+                }
+                .frame(height: 650)
+            }
         }
     }
-
 }
+
 #Preview {
     ranking()
 }
