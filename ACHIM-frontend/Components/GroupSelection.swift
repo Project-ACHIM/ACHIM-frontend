a8ba48f1775166d5e1b18accf0819f5beed399c1
りんご <hanacoi7243@gmail.com>
2025-07-01 09:21:57 +0900

    [add]monday group

---
+++ ACHIM-frontend/Components/GroupSelection.swift
@@ -0,0 +1,17 @@
+//
+//  Group.swift
+//  ACHIM-frontend
+//
+//  Created by 2230220 on 2025/06/30.
+//
+import SwiftUI
+
+struct Group: View {
+    var body: some View {
+        Text("Group")
+    }
+}
+
+#Preview {
+    Group()
+}
