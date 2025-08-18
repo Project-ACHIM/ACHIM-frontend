085d3fd070f94b2014c92a66973ceec7dc9d3db4
りんご <hanacoi7243@gmail.com>
2025-08-18 11:46:41 +0900

    [add] 称号

--- ACHIM-frontend/Views/SettingView.swift
+++ ACHIM-frontend/Views/SettingView.swift
@@ -12,11 +12,14 @@ struct SettingView: View {
             Color.white.ignoresSafeArea()
             
             VStack {
+                //アイコン
                 ZStack{
+                    //画像
                     Image(.myicon)
                         .resizable()
                         .frame(width: 150, height: 150)
                         .cornerRadius(100)
+                    //画像変更ボタン
                     VStack{
                         Spacer()
                         Button(action: {
@@ -32,11 +35,20 @@ struct SettingView: View {
                 }
                 .frame(width: 150, height: 150)
                 .padding(.top, 60)
+                
+                HStack{
+                   Text("最優秀賞")
+                        .zenFont(.regular, size: 12)
+                        .padding(.leading, 16)
+                        .padding(.trailing, 16)
+                        .background(Color.hexFBBC04)
+                        .cornerRadius(10)
+                }   // HStack
                 Spacer()
             }
-        }
-    }
-}
+        }   //ZStack
+    }   //body
+}   // View
 
 #Preview {
     SettingView()
